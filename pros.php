f6309c7a9398307d105a4d91a491cb4c7866ead0
Jasdero <houssemaine.j@gmail.com>
2016-11-05 18:24:05 +0100

    2è graph

--- pros.php
+++ pros.php
@@ -22,8 +22,9 @@
     <h1>Espace Simplifié</h1>
 <section>
     <div class="container ">
-        <h2>Résumé de ma semaine</h2>
+        <h2 class="">Résumé de ma semaine</h2>
             <div class="row column">
+                <div class="pull-left"> votes</div>
                 <div class="col-xs-1 first coll"></div>
                 <div class="col-xs-1 second coll"></div>
                 <div class="col-xs-1 third coll"></div>
@@ -40,7 +41,27 @@
                 <div class="col-xs-1 coll">Vendredi</div>
                 <div class="col-xs-1 coll">Samedi</div>
                 <div class="col-xs-1 coll">Dimanche</div>
+                <div class="col-xs-1 coll pull-right">Jour</div>
             </div>
+    </div>
+    <div class="container ">
+        <h2 class="">Résumé de mon dernier mois</h2>
+        <div class="row column">
+            <div class="pull-left"> votes</div>
+            <div class="col-xs-1 first coll"></div>
+            <div class="col-xs-1 second coll"></div>
+            <div class="col-xs-1 third coll"></div>
+            <div class="col-xs-1 fourth coll"></div>
+
+        </div>
+        <div class="row">
+            <div class="col-xs-1 coll">1ère semaine</div>
+            <div class="col-xs-1 coll">2è semaine</div>
+            <div class="col-xs-1 coll">3è semaine</div>
+            <div class="col-xs-1 coll">4è semaine</div>
+            <div class="col-xs-1 coll pull-right">Semaine</div>
+
+        </div>
     </div>
 </section>
 <footer></footer>
