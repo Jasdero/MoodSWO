720ee0ba074e5f91ad7a840e0cb73e896eb6ffe8
MathieuM1 <mat.mercier45@gmail.com>
2016-11-05 16:54:49 +0100

    bof

--- pros.php
+++ pros.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bars.css">
+    <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
